54acf6619655181de9bb49409989998af437e6be
kagenomusuko-svg <kage.no.musuko@gmail.com>
2026-09-18 02:18:03 -0600

    refactor: serve book assets directly from island

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/publicaciones',
-  async rewrites() {
-    return [
-      {
-        source: '/libros/afrodita-areia/volumen-I.pdf',
-        destination:
-          'https://raw.githubusercontent.com/kagenomusuko-svg/publicaciones/main/sobre-la-pasion/build/afrodita-areia-vol-i-sobre-la-pasion.pdf',
-      },
-      {
-        source: '/libros/afrodita-areia/portada.png',
-        destination:
-          'https://raw.githubusercontent.com/kagenomusuko-svg/publicaciones/main/sobre-la-pasion/Portada%20Vol%20I%20red.png',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
